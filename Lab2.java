2faa5a16ef3d3f34732c11a75d7cc9c6247a8477
Mariamyousaf27 <154213783+Mariamyousaf27@users.noreply.github.com>
2025-01-09 12:44:18 +0500

    Add files via upload

---
+++ Lab2.java
@@ -0,0 +1,26 @@
+
+
+package lab2;
+import java.util.*;
+
+public class Lab2 {
+
+   
+    public static void main(String[] args) {
+    Vector <String> vector = new Vector <String>(); //creating a vector
+    vector.add("python");
+    vector.add("java");
+    vector.add("JS");
+    vector.add("flutter");
+    vector.add("C++");
+    vector.add("C");
+            
+    
+    System.out.println("vectors added are:"+ vector);
+    
+    
+    
+       
+    }
+    
+}
---
+++ TASK2.java
@@ -0,0 +1,22 @@
+
+
+package lab2;
+import java.util.Vector;
+
+
+
+public class TASK2 {
+    public static void main (String[]args){
+        Vector <Integer> vector=new Vector<Integer>();
+        
+        for (int i=1;i <=10;i++){
+            vector.add(i);
+        }
+        System.out.println("Integer Vectors");
+        for(Integer number:numbers)
+        
+        
+    
+}
+    
+}
